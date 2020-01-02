fb48c82211ea7ae4891b89b52dac7e6556b076fb
scottie-schneider <scott@laclave.co>
2020-01-01 19:03:11 -0500

    test comment for dev branch

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from "react";
 // getting the URL we're running
 import absoluteUrl from 'next-absolute-url'
 
+// for dev
 // these are our authentication functions, which take care of grabbing the user object from either the server
 // or the client (where we store it on the window)
 import { getSessionFromClient, getSessionFromServer, redirectUser } from '../lib/auth'
