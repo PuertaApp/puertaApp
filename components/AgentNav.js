0da7875f9e88ec150362f8bb3752adb6990fa782
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2020-01-02 12:23:47 -0800

    THEY SHOW UP

--- components/AgentNav.js
+++ components/AgentNav.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import NavBar from './styles/NavBar';
 import NavButton from './styles/NavButton';
+import styled from 'styled-components';
+import HistoryIcon from '../components/icons/HistoryIcon';
+import ListIcon from '../components/icons/ListIcon';
 
+const StyledDiv = styled.div` 
+    margin: 33%
+`
 const AgentNav = () => {
     return(
         <NavBar>
-            <div></div>
-            <NavButton><a src='components/icons/ListIcon.js'/></NavButton>
+            <StyledDiv>
+            <NavButton><ListIcon/></NavButton>
             <div>Leads</div>
-            <NavButton><a src='./icons/HistoryIcon.js'/></NavButton>
+            </StyledDiv>
+            <StyledDiv>
+            <NavButton><HistoryIcon/></NavButton>
             <div>History</div>
+            </StyledDiv>
+            <StyledDiv>
             <NavButton>Profile</NavButton>
             <div>Profile</div>
+            </StyledDiv>
         </NavBar>
     )
 }
--- components/styles/NavBar.js
+++ components/styles/NavBar.js
@@ -2,10 +2,13 @@ import styled from 'styled-components';
 
 const NavBar = styled.div `
 display: flex;
-width: 600px;
+width: 100%;
 height: 120px;
 left: 1910px;
 top: 59px;
+font-size: 12px;
+justify-content: center;
+align-content: center;
 
 `
 
