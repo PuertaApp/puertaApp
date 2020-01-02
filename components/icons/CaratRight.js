56629039ca45c29473df64e2ba79d4065c9eb38b
scottie-schneider <scott@laclave.co>
2020-01-02 13:05:57 -0500

    icons

---
+++ components/icons/CaratRight.js
@@ -0,0 +1,6 @@
+const CaratRight = (props) => (
+  <svg width="21" height="32" viewBox="0 0 21 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M4.01 0L20.01 16L4.01 32L0 28L12.01 16L0 4L4.01 0Z" fill="#141940"/>
+  </svg>
+)
+export default CaratRight;
