458da42fed78a25360830771dc2faaa84b509b69
scottie-schneider <scott@laclave.co>
2020-01-02 18:54:51 -0500

    changes

--- components/styles/FloatingSearchForm.js
+++ components/styles/FloatingSearchForm.js
@@ -5,11 +5,15 @@ const FloatingSearchForm = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   align-items: center;
+  text-align: center;
   justify-content: center;
   width: 100vw;    
   height: 100vh;
   margin: 0 auto;
   overflow: hidden;
+  h1 {
+    font-size: 3rem;
+  }
   .search-form {
     padding: 2rem 1.5rem 2rem 1.5rem;
     border-radius: 5px;
