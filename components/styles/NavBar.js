ca6fb37cc594a9823d9f5b804333e22ad5c55af7
SebastianGarces <gsgarces1@gmail.com>
2020-01-03 00:04:19 -0500

    fixed navbar styled component typo

--- components/styles/NavBar.js
+++ components/styles/NavBar.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
-<<<<<<< HEAD
 const NavBar = styled.div`
+	z-index: 1000;
 	display: flex;
 	position: fixed;
 	justify-content: space-evenly;
@@ -12,7 +12,6 @@ const NavBar = styled.div`
 	width: 100%;
 	max-height: 120px;
 	min-height: 70px;
-	z-index: 9999;
 	@media (min-width: 900px) {
 		top: 0;
 		padding-bottom: 80px;
@@ -20,25 +19,4 @@ const NavBar = styled.div`
 		justify-content: flex-end;
 	}
 `;
-=======
-const NavBar = styled.div `
-z-index: 1000;
-display: flex;
-position: fixed;
-justify-content: space-evenly;
-align-items: center;
-flex: 1;
-bottom: 0;
-margin: 0;
-width: 100%;
-max-height: 120px;
-min-height: 70px;
-@media (min-width:900px){
-    top:0;
-    padding-bottom: 80px;
-    
-    justify-content: flex-end;
-}
->>>>>>> 21084677c115c83e8fe9434f51d1f03354be5252
-
 export default NavBar;
