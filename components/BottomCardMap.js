01a2dcd4e129960bdc4acd206f8e4771673e536f
SebastianGarces <gsgarces1@gmail.com>
2020-01-02 23:57:32 -0500

    added icons and finalized sizing

--- components/BottomCardMap.js
+++ components/BottomCardMap.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import MapMarkerIcon from "../components/icons/MapMarkerIcon";
+import SearchIcon from "../components/icons/SearchIcon";
 
 const BottomCardMapStyle = styled.div`
 	display: flex;
@@ -11,29 +13,29 @@ const BottomCardMapStyle = styled.div`
 	position: fixed;
 	justify-content: space-evenly;
 	align-items: center;
-	padding: 0 3rem 70px;
+	padding: 0 1rem 70px;
 	flex: 1;
 	bottom: 0;
 	margin: 0;
 	width: 100%;
 	min-height: 120px;
-  height: 20vh;
-	border: 1px solid blue;
 	h3 {
 		text-align: center;
+		font-size: 0.9rem;
+		margin: 0.5rem;
 	}
 	ul {
-		border: 1px solid red;
 		display: flex;
 		width: 100%;
 		flex-direction: column;
 		justify-content: center;
-		align-items: flex-end;
+		align-items: center;
 		padding: 0;
 		margin: 0;
 		li {
 			display: flex;
 			margin: 0.2rem 0;
+			align-items: center;
 			input {
 				background: #f9f9f9;
 				border: 3px solid #ffffff;
@@ -42,12 +44,30 @@ const BottomCardMapStyle = styled.div`
 					inset 2px 2px 8px rgba(126, 138, 167, 0.4),
 					inset -4px -4px 10px rgba(255, 255, 255, 0.7);
 				border-radius: 26px;
-				padding: 0.8rem 1rem;
+				padding: 0.6rem 2rem;
 
 				&::placeholder {
 					color: rgba(150, 165, 199, 0.5);
-					font-size: 1rem;
+					font-size: 0.8rem;
 				}
+			}
+			.icon {
+				background: linear-gradient(
+					131.71deg,
+					#fafafa 0%,
+					#f5f5f5 100.03%
+				);
+				border: 1px solid rgba(255, 255, 255, 0.6);
+				box-sizing: border-box;
+				box-shadow: 4px 4px 8px rgba(170, 182, 209, 0.4),
+					-6px -6px 12px rgba(255, 255, 255, 0.8);
+				border-radius: 15px;
+				height: 40px;
+				width: 40px;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				margin-left: 0.5rem;
 			}
 		}
 	}
@@ -64,7 +84,9 @@ const BottomCardMap = props => (
 					id="name"
 					placeholder="Please enter your full name..."
 				/>
-				<div className="icon"></div>
+				<div className="icon">
+					<SearchIcon height={"25px"} width={"25px"} />
+				</div>
 			</li>
 			<li>
 				<input
@@ -73,7 +95,9 @@ const BottomCardMap = props => (
 					id="phone-number"
 					placeholder="Please enter your phone number..."
 				/>
-				<div className="icon"></div>
+				<div className="icon">
+					<MapMarkerIcon height={"25px"} width={"25px"} />
+				</div>
 			</li>
 		</ul>
 	</BottomCardMapStyle>
