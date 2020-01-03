61b91752d287981d7b28caf40dae079f3def34d3
SebastianGarces <gsgarces1@gmail.com>
2020-01-02 23:26:38 -0500

    added zindex no navbar

--- components/styles/NavBar.js
+++ components/styles/NavBar.js
@@ -1,24 +1,23 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-const NavBar = styled.div `
+const NavBar = styled.div`
+	display: flex;
+	position: fixed;
+	justify-content: space-evenly;
+	align-items: center;
+	flex: 1;
+	bottom: 0;
+	margin: 0;
+	width: 100%;
+	max-height: 120px;
+	min-height: 70px;
+	z-index: 9999;
+	@media (min-width: 900px) {
+		top: 0;
+		padding-bottom: 80px;
 
-display: flex;
-position: fixed;
-justify-content: space-evenly;
-align-items: center;
-flex: 1;
-bottom: 0;
-margin: 0;
-width: 100%;
-max-height: 120px;
-min-height: 70px;
-@media (min-width:900px){
-    top:0;
-    padding-bottom: 80px;
-    
-    justify-content: flex-end;
-}
-
-`
+		justify-content: flex-end;
+	}
+`;
 
 export default NavBar;
