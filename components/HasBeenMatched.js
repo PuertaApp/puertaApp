00cceafb622ee2da70b699d17d9985977e6c07a4
SebastianGarces <gsgarces1@gmail.com>
2020-01-02 21:55:05 -0500

    made adjustments to hasbeenmatched component

--- components/HasBeenMatched.js
+++ components/HasBeenMatched.js
@@ -79,6 +79,14 @@ const StyledHasBeenMatched = styled.div`
 			padding: 0.8rem 2rem;
 			color: rgba(20, 25, 64, 1);
 			font-size: 1.3rem;
+
+			&:active {
+				background: #f9f9f9;
+				border: 3px solid #ffffff;
+				box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1),
+					inset 2px 2px 8px rgba(126, 138, 167, 0.4),
+					inset -4px -4px 10px rgba(255, 255, 255, 0.7);
+			}
 		}
 	}
 `;
