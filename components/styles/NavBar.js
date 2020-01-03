862cf34897a0b2cfa6c9d253d9bae884db47e260
SebastianGarces <gsgarces1@gmail.com>
2020-01-03 04:19:01 -0500

    added z-index to NavBar

--- components/styles/NavBar.js
+++ components/styles/NavBar.js
@@ -1,24 +1,24 @@
 import styled from "styled-components";
 
-const NavBar = styled.div `
-background: #f9f9f9;
-display: flex;
-position: fixed;
-justify-content: space-evenly;
-align-items: center;
-flex: 1;
-bottom: 0;
-margin: 0;
-width: 100%;
-max-height: 120px;
-min-height: 70px;
-@media (min-width:900px){
-    top:0;
-    padding-bottom: 80px;
-    
-    justify-content: flex-end;
-}
+const NavBar = styled.div`
+	background: #f9f9f9;
+	display: flex;
+	position: fixed;
+	justify-content: space-evenly;
+	align-items: center;
+	flex: 1;
+	bottom: 0;
+	margin: 0;
+	width: 100%;
+	max-height: 120px;
+	min-height: 70px;
+	z-index: 9999;
+	@media (min-width: 900px) {
+		top: 0;
+		padding-bottom: 80px;
 
-`
+		justify-content: flex-end;
+	}
+`;
 
 export default NavBar;
